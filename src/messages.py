41017ad2e0efc3db4ebd0d09795acc75f61802af
Oct-Lee <954250092@qq.com>
2026-07-22 10:22:31 +0800

    Add localization CI test

---
+++ src/messages.py
@@ -0,0 +1,14 @@
+CAMERA_NOT_FOUND_ERROR = (
+    "camera[{camera_id}] not Founded，Please check whether the "
+    "configration file is correct"
+)
+
+
+CAMERA_START_ERROR = (
+    "Camera have started sucessfully"
+)
+
+
+NETWORK_ERROR = (
+    "Can not connect to server"
+)
