f0bfcc560a27b50baa7c37a78840695279d7b48c
Oct-Lee <954250092@qq.com>
2026-07-22 11:18:36 +0800

    Fix localization quality gate workflow

--- src/messages.py
+++ src/messages.py
@@ -1,4 +1,4 @@
-CAMERA_NOT_FOUND_ERROR = "camera[{camera_id}] not Founded. Please check whether the 'camera_id' parameter of the configration file is correct"
+
 text = """
 Camera have started.
 Camera are connected.
